17df2f67eedbaeb08c02ea7fe26aa0e9964d970a
zachpuck <zachpuckett@gmail.com>
2018-12-14 12:15:49 -0800

    add commands to enable password login as root

--- .opspec/build-azure/create-airgap-infra/enable-root.sh
+++ .opspec/build-azure/create-airgap-infra/enable-root.sh
@@ -29,9 +29,17 @@ cpIP=$(az vm show -g $resourceGroup -n $name-cp -d --query publicIps --out tsv)
 echo "enable ssh as root on control plane"
 ssh -o "StrictHostKeyChecking no" -i /privateKey centos@$cpIP \
     'export TERM=xterm;
-    sudo sed -i s/^#PermitRootLogin\ yes/PermitRootLogin\ without-password/ /etc/ssh/sshd_config;
-    sudo mkdir /root/.ssh;
-    sudo cp ~/.ssh/authorized_keys /root/.ssh'
+    sudo sed -e "s/^#PermitRootLogin\ yes/PermitRootLogin\ yes/" \
+             -e "s/^PasswordAuthentication no/PasswordAuthentication yes/" -i /etc/ssh/sshd_config;
+    sudo mkdir -p /root/.ssh;
+    sudo cp ~/.ssh/authorized_keys /root/.ssh;
+    sudo systemctl restart sshd'
+
+
+echo "setting root password on control plane"
+ssh -o "StrictHostKeyChecking no" -i /privateKey root@$cpIP \
+    "export TERM=xterm
+    echo "${rootPassword}" | passwd --stdin root"
 
 echo "get node public IP"
 nodeIP=$(az vm show -g $resourceGroup -n $name-node -d --query publicIps --out tsv)
@@ -39,6 +47,13 @@ nodeIP=$(az vm show -g $resourceGroup -n $name-node -d --query publicIps --out t
 echo "enable ssh as root on node"
 ssh -o "StrictHostKeyChecking no" -i /privateKey centos@$nodeIP \
     'export TERM=xterm;
-    sudo sed -i s/^#PermitRootLogin\ yes/PermitRootLogin\ without-password/ /etc/ssh/sshd_config;
-    sudo mkdir /root/.ssh;
-    sudo cp ~/.ssh/authorized_keys /root/.ssh'
+    sudo sed -e "s/^#PermitRootLogin\ yes/PermitRootLogin\ yes/" \
+             -e "s/^PasswordAuthentication no/PasswordAuthentication yes/" -i /etc/ssh/sshd_config;
+    sudo mkdir -p /root/.ssh;
+    sudo cp ~/.ssh/authorized_keys /root/.ssh;
+    sudo systemctl restart sshd'
+
+echo "setting root password on node"
+ssh -o "StrictHostKeyChecking no" -i /privateKey root@$nodeIP \
+    "export TERM=xterm;
+    echo -e "${rootPassword}" | passwd --stdin root"
