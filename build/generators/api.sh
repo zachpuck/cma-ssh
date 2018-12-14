1f0de63030e8897cf4fa9db5844fd564c3ff8312
Marat Garafutdinov <maratoid@gmail.com>
2018-12-14 09:38:35 -0800

    fix docker build

--- build/generators/api.sh
+++ build/generators/api.sh
@@ -16,20 +16,20 @@ mkdir -p ${PROJECT_DIRECTORY}/${SWAGGER_DESTINATION}
 
 echo
 echo "generating api stubs..."
-echo "protoc ${PROJECT_DIRECTORY}/api/api.proto -I ${PROJECT_DIRECTORY}/api/  --go_out=plugins=grpc:${PROJECT_DIRECTORY}/${DESTINATION_LIBRARY}/ -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway -I$GOPATH/src"
-protoc ${PROJECT_DIRECTORY}/api/api.proto -I ${PROJECT_DIRECTORY}/api/  --go_out=plugins=grpc:${PROJECT_DIRECTORY}/${DESTINATION_LIBRARY}/ -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway -I$GOPATH/src
+echo "protoc ${PROJECT_DIRECTORY}/api/api.proto -I ${PROJECT_DIRECTORY}/api/  --go_out=plugins=grpc:${PROJECT_DIRECTORY}/${DESTINATION_LIBRARY}/ -I${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway -I${PROJECT_DIRECTORY}/vendor"
+protoc ${PROJECT_DIRECTORY}/api/api.proto -I ${PROJECT_DIRECTORY}/api/  --go_out=plugins=grpc:${PROJECT_DIRECTORY}/${DESTINATION_LIBRARY}/ -I${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway -I${PROJECT_DIRECTORY}/vendor
 
 echo
 echo "generating REST gateway stubs..."
-echo "protoc ${PROJECT_DIRECTORY}/api/api.proto -I /usr/local/include/ -I ${PROJECT_DIRECTORY}/api/ -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I $GOPATH/src/github.com//grpc-ecosystem/grpc-gateway --grpc-gateway_out=logtostderr=true:${PROJECT_DIRECTORY}/${DESTINATION_LIBRARY}"
-protoc ${PROJECT_DIRECTORY}/api/api.proto -I /usr/local/include/ -I ${PROJECT_DIRECTORY}/api/ -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I $GOPATH/src/github.com//grpc-ecosystem/grpc-gateway --grpc-gateway_out=logtostderr=true:${PROJECT_DIRECTORY}/${DESTINATION_LIBRARY}
+echo "protoc ${PROJECT_DIRECTORY}/api/api.proto -I /usr/local/include/ -I ${PROJECT_DIRECTORY}/api/ -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I ${PROJECT_DIRECTORY}/vendor/github.com//grpc-ecosystem/grpc-gateway --grpc-gateway_out=logtostderr=true:${PROJECT_DIRECTORY}/${DESTINATION_LIBRARY}"
+protoc ${PROJECT_DIRECTORY}/api/api.proto -I /usr/local/include/ -I ${PROJECT_DIRECTORY}/api/ -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I ${PROJECT_DIRECTORY}/vendor/github.com//grpc-ecosystem/grpc-gateway --grpc-gateway_out=logtostderr=true:${PROJECT_DIRECTORY}/${DESTINATION_LIBRARY}
 
 echo
 echo "generating swagger docs..."
-echo "protoc ${PROJECT_DIRECTORY}/api/api.proto -I /usr/local/include/ -I ${PROJECT_DIRECTORY}/api/ -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway --swagger_out=logtostderr=true:${PROJECT_DIRECTORY}/${SWAGGER_DESTINATION}"
-protoc ${PROJECT_DIRECTORY}/api/api.proto -I /usr/local/include/ -I ${PROJECT_DIRECTORY}/api/ -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway --swagger_out=logtostderr=true:${PROJECT_DIRECTORY}/${SWAGGER_DESTINATION}
+echo "protoc ${PROJECT_DIRECTORY}/api/api.proto -I /usr/local/include/ -I ${PROJECT_DIRECTORY}/api/ -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway --swagger_out=logtostderr=true:${PROJECT_DIRECTORY}/${SWAGGER_DESTINATION}"
+protoc ${PROJECT_DIRECTORY}/api/api.proto -I /usr/local/include/ -I ${PROJECT_DIRECTORY}/api/ -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway --swagger_out=logtostderr=true:${PROJECT_DIRECTORY}/${SWAGGER_DESTINATION}
 
 echo
 echo "generating api docs..."
-echo "protoc ${PROJECT_DIRECTORY}/api/api.proto -I ${PROJECT_DIRECTORY}/api/ -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway ${PROJECT_DIRECTORY}/api/api.proto --doc_out ${PROJECT_DIRECTORY}/docs/api-generated --doc_opt=markdown,api.md"
-protoc ${PROJECT_DIRECTORY}/api/api.proto -I ${PROJECT_DIRECTORY}/api/ -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I $GOPATH/src/github.com/grpc-ecosystem/grpc-gateway ${PROJECT_DIRECTORY}/api/api.proto --doc_out ${PROJECT_DIRECTORY}/docs/api-generated --doc_opt=markdown,api.md
+echo "protoc ${PROJECT_DIRECTORY}/api/api.proto -I ${PROJECT_DIRECTORY}/api/ -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway ${PROJECT_DIRECTORY}/api/api.proto --doc_out ${PROJECT_DIRECTORY}/docs/api-generated --doc_opt=markdown,api.md"
+protoc ${PROJECT_DIRECTORY}/api/api.proto -I ${PROJECT_DIRECTORY}/api/ -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis -I ${PROJECT_DIRECTORY}/vendor/github.com/grpc-ecosystem/grpc-gateway ${PROJECT_DIRECTORY}/api/api.proto --doc_out ${PROJECT_DIRECTORY}/docs/api-generated --doc_opt=markdown,api.md
