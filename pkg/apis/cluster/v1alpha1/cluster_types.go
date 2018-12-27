8f8f12db72e6082adc2b0f05ac240393e1955fa6
zachpuck <zachpuckett@gmail.com>
2018-12-27 13:34:08 -0800

    add status and age columns

--- pkg/apis/cluster/v1alpha1/cluster_types.go
+++ pkg/apis/cluster/v1alpha1/cluster_types.go
@@ -78,6 +78,8 @@ type APIEndpoint struct {
 
 // Cluster is the Schema for the clusters API
 // +k8s:openapi-gen=true
+// +kubebuilder:printcolumn:name="Status",type="string",JSONPath=".status.phase",description="machine status"
+// +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 type CnctCluster struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
